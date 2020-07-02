09023ecb263d6b13eae710797e1e5d42c6ed7c65
Francisco Ozuna <ozuna252@hotmail.com>
2020-07-01 20:32:39 -0400

    Initialized buttons and edit text along with onClick listeners

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.lab6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private Button startServiceButton;
+    private Button stopServiceButton;
+    private EditText randomCharacterEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startServiceButton = (Button) findViewById(R.id.button_start);
+        stopServiceButton = (Button) findViewById(R.id.button_end);
+
+        randomCharacterEditText = (EditText) findViewById(R.id.editText_randomCharacter);
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_start:
+                break;
+            case R.id.button_end:
+                break;
+        }
+    }
+
+
 }
