b2aed97b1151e8ffce71a8dcd6eacf6287055796
Francisco Ozuna <ozuna252@hotmail.com>
2020-07-02 00:04:11 -0400

    Added code so that main activity is registered to intercept the broadcast and displays random characters in edit text

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,36 +2,78 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = "MainActivity";
-    private Button startServiceButton;
-    private Button stopServiceButton;
     private EditText randomCharacterEditText;
+
+    private BroadcastReceiver broadcastReceiver;
+
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startServiceButton = (Button) findViewById(R.id.button_start);
-        stopServiceButton = (Button) findViewById(R.id.button_end);
+        randomCharacterEditText = findViewById(R.id.editText_randomCharacter);
 
-        randomCharacterEditText = (EditText) findViewById(R.id.editText_randomCharacter);
+        broadcastReceiver = new MyBroadcastReceiver();
+
+        serviceIntent = new Intent(getApplicationContext(), RandomCharacterService.class);
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_start:
+                startService(serviceIntent);
                 break;
             case R.id.button_end:
+                stopService(serviceIntent);
+                randomCharacterEditText.setText("");
                 break;
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("my.custom.action.tag.lab6");
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    class MyBroadcastReceiver extends BroadcastReceiver {
+        String TAG = "MyBroadcastReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+
+                Log.i(TAG, "onReceive() called");
+                char data = intent.getCharExtra("randomCharacter", '?');
+
+                Log.i(TAG, "Random Character is: " + data);
+                randomCharacterEditText.setText(String.valueOf(data));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
+
+
